47166f8a52477dbfe4134e8340966a9a73c05081
SimonDemarty <simon@demarty.org>
2024-09-05 11:40:06 +0100

    add: base code to store music

--- main.py
+++ main.py
@@ -1,6 +1,68 @@
+# Dependencies ================================================================
 import os
 from dotenv import load_dotenv, dotenv_values
+import copy
 
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
+
+import track
+
+# Load environment variables ==================================================
 load_dotenv()
 
-print(os.getenv("MY_KEY"))
+SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+SPOTIFY_REDIRECT_URI = os.getenv("SPOTIFY_REDIRECT_URI")
+
+print("SPOTIFY_CLIENT_ID:       %s"%(SPOTIFY_CLIENT_ID))
+print("SPOTIFY_CLIENT_SECRET:   %s"%(SPOTIFY_CLIENT_SECRET))
+print("SPOTIFY_REDIRECT_URI:    %s"%(SPOTIFY_REDIRECT_URI))
+
+# Initiate Spotify connection =================================================
+
+# cf. https://developer.spotify.com/documentation/web-api/concepts/scopes
+scope="playlist-read-private,playlist-read-collaborative,user-library-read"
+
+auth_manager = SpotifyOAuth(
+    client_id=SPOTIFY_CLIENT_ID,
+    client_secret=SPOTIFY_CLIENT_SECRET,
+    redirect_uri=SPOTIFY_REDIRECT_URI,
+    scope=scope
+)
+sp = spotipy.Spotify(auth_manager=auth_manager)
+sp2 = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
+    client_id=SPOTIFY_CLIENT_ID,
+    client_secret=SPOTIFY_CLIENT_SECRET,
+))
+
+
+# Actual code =================================================================
+
+# Get user
+user = sp.me()
+
+# Get all saved tracks
+
+saved_tracks = []
+
+iteration = 0
+browse_all_saved_tracks = False
+while not browse_all_saved_tracks:
+
+    offset = iteration*20
+    results=sp.current_user_saved_tracks(offset=offset)
+
+
+
+    for idx, item in enumerate(results['items']):
+
+        my_track = track.track(item)
+        my_track.display()
+
+        saved_tracks.append(my_track)
+        
+
+    iteration += 1
+    if len(results['items']) < 20 :
+        browse_all_saved_tracks = True
---
+++ track.py
@@ -0,0 +1,48 @@
+class track:
+    def __init__(self, spotify_track):
+
+        self.spotify_JSON = spotify_track
+        
+        # Track information
+        self.spotify_id = spotify_track['track']['id']
+        self.isrc = spotify_track['track']['external_ids']['isrc'] if 'isrc' in spotify_track['track']['external_ids'] else None
+        self.ean = spotify_track['track']['external_ids']['ean'] if 'ean' in spotify_track['track']['external_ids'] else None
+        self.upc = spotify_track['track']['external_ids']['upc'] if 'upc' in spotify_track['track']['external_ids'] else None
+
+        self.name = spotify_track['track']['name']
+
+        self.duration = spotify_track['track']['duration_ms']
+
+        self.artists_spotify_ids = [artist['id'] for artist in spotify_track['track']['artists']]
+        self.artists_names = [artist['name'] for artist in spotify_track['track']['artists']]
+        
+
+        # Album information
+        # TODO: create album class
+        self.album_id = spotify_track['track']['album']['id']
+
+        self.album_type = spotify_track['track']['album']['album_type']
+        self.album_type2 = spotify_track['track']['album']['type']
+        
+        self.album_name = spotify_track['track']['album']['name']
+        self.album_release_date = spotify_track['track']['album']['release_date']
+        self.album_image = spotify_track['track']['album']['images'][0]
+
+        self.disc_number = spotify_track['track']['disc_number']
+        self.track_number = spotify_track['track']['track_number']
+
+        self.album_artists_spotify_ids = [artist['id'] for artist in spotify_track['track']['album']['artists']]
+        self.album_artists_name = [artist['name'] for artist in spotify_track['track']['album']['artists']]
+
+    def display(self) :
+        artists = ""
+        for artist in self.artists_names[:-1]:
+            artists +=  artist + ", "
+        artists += self.artists_names[-1]
+
+        duration = str(int((self.duration/(1000*60*60))%24)) + ":" + str(int((self.duration/(1000*60))%60)) + ":" + str(int((self.duration/1000)%60))
+
+        print(artists + " - " + self.name + " - " + self.album_name + " - " + duration + " - " + str(self.isrc) + "|" + str(self.ean) + "|" + str(self.upc) + " - " + self.spotify_id)
+    
+    def display_JSON(self) :
+        print(self.spotify_JSON)
